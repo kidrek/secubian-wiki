8906d95bfdb5cb7df8687492513902794c7eb3a7
Kidr ek <kidrek@gmail.com>
2020-02-02 13:19:42 +0100

    1er commit de l'intégralité de dokuwiki

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+secubian:$1$sLgL8QnG$js9.Ck.UQFHJYbqyPnLzw0:secubian:secubian@localhost.local:admin,user
---
+++ lib/tpl/sprintdoc/conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * default configuration settings
+ *
+ */
+
+$conf['sidebar_sections'] = 'h1,h2,h3,h4,h5';
+$conf['header_layout'] = 'header-default';
